66264adf5ff9387185f88228f7d5ee0f9ebe7e0d
DaheeJang <jecydahee77@gmail.com>
2025-03-20 14:00:50 +0900

    Learn section4

--- learn-react/src/components/course/CourseListCard.jsx
+++ learn-react/src/components/course/CourseListCard.jsx
@@ -1,10 +1,11 @@
-import React from 'react';
 import CourseItem from './CourseItem'
 
 function CourseListCard(props) {
   return (
-    <div className="card">
-          <div className="card__header">강의 목록</div>
+    <div className="card" style={{backgroundColor: 'black', color: 'white'}}>
+      {/* <div className="card" style="background-color: black; color: white"> // 안됨  */}
+          <div className="card__header">강의 목록</div> 
+          <div className="card_header">강의 목록</div>
           <div className="card__body">
           <div className="courses">
               <CourseItem />
