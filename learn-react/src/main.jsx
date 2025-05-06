7ba7e485e7b71fb0928fdd82f4050d02f33d2ae2
Dahee Jang <122618383+jecy77@users.noreply.github.com>
2025-05-06 23:32:40 +0900

    Update main.jsx

--- learn-react/src/main.jsx
+++ learn-react/src/main.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// import App from './AppTodo.jsx'
+import App from './AppTodo.jsx'
 // import App from './AppPure.jsx'
 // import App from './App.jsx'
 // import App from './AppCounter.jsx'
 // import App from './AppMovingDot'
 // import App from './AppCourse'
-import App from "./AppTheme";
+// import App from "./AppTheme";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <AppTodo />
   </React.StrictMode>,
 );
